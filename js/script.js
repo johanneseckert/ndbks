ecb4c5bef0248abf51a74957840846867d744019
Johannes Henseler <frischmilch@gmail.com>
2011-02-10 12:19:10 +0100

    + yourls URL shortener
    * changed UI theme
    
    * enhanced builder
    
    
    Signed-off-by: Johannes Henseler <frischmilch@gmail.com>

--- js/script.js
+++ js/script.js
@@ -125,6 +125,7 @@ function builder() {
 	this.components.cranks = new component("cranks","cr",["black","gold","silver"],"silver");
 	this.components.grips = new component("grips","gr",["black","brown","white"],"brown");
 	this.components.chain = new component("chain","ch",["white","black","red","yellow"],"black");
+	this.components.handlebar = new component("handlebar","hb-riserbar",["black"]);
 	this.components.saddle = new component("saddle","sd",["white","brown","black"],"brown");
 	this.components.tirefront = new component("tirefront","trf",["red","black","yellow","white"],"yellow");
 	this.components.rimfront = new component("rimfront","rmf",["red","black","white"],"white");
@@ -150,7 +151,7 @@ function builder() {
 
 		// push new bike state into the URL
 		$.bbq.pushState( bike.components );
-			
+		
 	}
 
 	this.nextcolor = function(what) {
@@ -162,12 +163,18 @@ function builder() {
 			next_colorindex = 0;
 		
 		this.update(what,this.components[what].colors[next_colorindex]);
-
-
-		log("trying to find next color on "+what);
-		log(this.components[what].colors.length);
-		log("current: "+$.inArray(this.components[what].color,this.components[what].colors))
 	}
+
+	this.shuffle = function() {
+		$.each(this.components, function(name,value) {
+			max_colorindex = bike.components[name].colors.length;
+			// random: http://www.shawnolson.net/a/789/make-javascript-mathrandom-useful.html
+			random_color = Math.floor(Math.random()*max_colorindex);
+			color_at_this_index = bike.components[name].colors[random_color];
+			bike.update(name,color_at_this_index);
+//			log(name + " / max_colorindex "+max_colorindex+" / random "+random_color+" - "+color_at_this_index);
+		});
+	};
 
 	this.displaycomponents = function() {
 		// firstly, hide all components to start fresh
@@ -201,6 +208,10 @@ function component(title,titleshort,colors,color) {
 // preload images in builder
 // and update bike after finish
 
+$("#preload_progressbar").progressbar({
+	value: 1
+});
+
 $.preload( "#builder_image img", {
 	onRequest:update,
 	onFinish:finish,
@@ -208,10 +219,18 @@ $.preload( "#builder_image img", {
 	threshold: 2 //'2' is the default, how many at a time, to load.
 });
 function update(data) {
-	$("#loaded").html(''+data.loaded);
-	$("#done").html(''+data.done);
-	$("#failed").html(''+data.failed);
-	$("#total").html(''+data.total);
+	percentage = Math.round((data.done / data.total)*100);
+
+	//getter
+	var value = $( "#preload_progressbar" ).progressbar( "option", "value" );
+	//setter
+	$( "#preload_progressbar" ).progressbar( "option", "value", percentage );
+
+//	$("#loaded").html(''+data.loaded);
+//	$("#done").html(''+data.done);
+//	$("#failed").html(''+data.failed);
+//	$("#total").html(''+data.total);
+//	$("#perc").html(''+percentage);
 };
 function finish() {
 	bike.displaycomponents();
@@ -240,4 +259,64 @@ $("#builder_image AREA").click(function(e) {
 	bike.nextcolor($(e.target).attr("alt"));
 
 	return false;
+});
+
+$("#btn_random").click(function() {
+	bike.shuffle();
+});
+
+
+ // Bind an event to window.onhashchange that, when the history state changes,
+ // gets the url from the hash and displays either our cached content or fetches
+ // new content to be displayed.
+ $(window).bind( 'hashchange', function(e) {
+// 	bike.components = $.bbq.getState();
+// 	bike.displaycomponents();
+	// at least reset sharing
+	$("#shared_info").slideUp();
+	$("#click_here_to_share").slideDown();
+ });
+
+
+
+// sharing with the YOURLS api
+// http://testserver2.dyndns.org/b/yourls-api.php?
+//	signature=8e9cd31548&action=shorturl&url=google.com&
+
+$("#click_here_to_share").click( function() {
+
+	$("#click_here_to_share").slideUp();
+	$("#shared_load").slideDown();
+
+	$.get("/b/yourls-api.php?signature=8e9cd31548&action=shorturl"
+			+"&url="+escape(location.href)
+			+"&format=simple",function(data) {
+
+		$("#shared_load").slideUp();
+		$("#shared_info").slideDown();
+
+		$("#shared_url").val(data);
+//		$("#shared_url").focus().val($("#shared_url").val);
+
+		log($("#social_fb"));
+
+		redirect_to = data;
+//		redirect_to = "http://indiebikes.com/";
+
+		$("#social_fb").attr("href","http://www.facebook.com/dialog/feed?app_id=197627106915572&redirect_uri="+redirect_to
+		+"&message=I desined my own bike!"
+		+"&link="+data
+		+"&picture=http://testserver2.dyndns.org/images/fb_image.png");
+
+		$("#social_twitter").attr("href","http://twitter.com/share?original_referer=http://indiebikes.com/&text=I%20designed%20my%20own%20bike!"
+		+"&url="+data
+		+"&via=indiebikes"
+		+"&counturl="+escape(location.href));
+	});
+
+	return false;
+});
+
+$("#shared_url").click(function() {
+	$(this).select();
 });
