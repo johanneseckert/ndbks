5b5f574bc48089ec45a7e54d5e363b1a02982ec1
Johannes Henseler <frischmilch@gmail.com>
2011-02-10 12:52:34 +0100

    - removed & ignore config files (for security reasons, obviously)

--- textpattern/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$txpcfg['db'] = 'indiebikes';
-$txpcfg['user'] = 'root';
-$txpcfg['pass'] = '';
-$txpcfg['host'] = 'localhost';
-$txpcfg['table_prefix'] = '';
-$txpcfg['txpath'] = 'C:\xampplite\htdocs\indiebikes\textpattern';
-$txpcfg['dbcharset'] = 'utf8';
-?>
